4c95a74e1bc0ecc01a2b2ca604380bac859d10e8
yinpinghui <yinpinghui@gmail.com>
2013-06-18 18:04:44 +0800

    update error

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,9 @@
 exports.routes = function (map) {
     map.resources('lists');
-    map.post()
-    map.delel
+
     // Generic routes. Add all your routes below this line
     // feel free to remove generic routes
     map.all(':controller/:action');
     map.all(':controller/:action/:id');
-    map.all(*, "con1, _404action")
+    
 };
