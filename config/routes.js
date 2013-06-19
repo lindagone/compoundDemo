888233b33d4e9591cb4a7732dc8c41dbd1e83b9a
yinpinghui <yinpinghui@gmail.com>
2013-06-19 17:55:12 +0800

    merge lindagone

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,9 @@
 exports.routes = function (map) {
     map.resources('lists');
+
     // Generic routes. Add all your routes below this line
     // feel free to remove generic routes
     map.all(':controller/:action');
     map.all(':controller/:action/:id');
+
 };
